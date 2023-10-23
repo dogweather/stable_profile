1b1a62a5853d3a951af017a6a799807ebee605e8
Robb Shecter <robb@public.law>
2023-10-23 01:14:30 -0600

    all tests pass

--- spec/stable_profile_spec.rb
+++ spec/stable_profile_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe StableProfile do
   it "has a version number" do
     expect(StableProfile::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
