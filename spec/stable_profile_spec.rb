b41cecdc231c50cdb38e41a5507fbc9f8ab17e8e
Robb Shecter <robb@public.law>
2023-10-30 19:04:30 -0600

    tests pass (#2)

--- spec/stable_profile_spec.rb
+++ spec/stable_profile_spec.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 RSpec.describe StableProfile do
-  it "has a version number" do
-    expect(StableProfile::VERSION).not_to be nil
-  end
-
   describe "StableProfile::OUTPUT_DIR" do
     subject { StableProfile::OUTPUT_DIR }
     it { is_expected.to be_a String }
